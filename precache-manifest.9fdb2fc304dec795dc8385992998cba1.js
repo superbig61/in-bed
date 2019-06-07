d53cc3ed24ba3e8219659d44418f396f7b076780
superbig61 <mihara.naoto.p0@tufs.ac.jp>
2019-06-07 19:19:17 +0200

    Updates

---
+++ precache-manifest.9fdb2fc304dec795dc8385992998cba1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e76bb03f4184fc4804b0beb11ca70f3",
+    "url": "www.in-bed.org/index.html"
+  },
+  {
+    "revision": "0667bc0d82f3ac228ed7",
+    "url": "www.in-bed.org/static/css/main.1037e156.chunk.css"
+  },
+  {
+    "revision": "a234a26be97fb38bea0c",
+    "url": "www.in-bed.org/static/js/2.9259a5be.chunk.js"
+  },
+  {
+    "revision": "0667bc0d82f3ac228ed7",
+    "url": "www.in-bed.org/static/js/main.c6101445.chunk.js"
+  },
+  {
+    "revision": "8b3218d455204ac4d87d",
+    "url": "www.in-bed.org/static/js/runtime~main.4f663c60.js"
+  },
+  {
+    "revision": "1e5549a999a2c5f2462b3e73f6549096",
+    "url": "www.in-bed.org/static/media/about.1e5549a9.jpg"
+  },
+  {
+    "revision": "2c940f81cd724a0ad5ba97b4c83ed337",
+    "url": "www.in-bed.org/static/media/home.2c940f81.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2bfedbc95b458d9a958f26199031060.js"
+  "www.in-bed.org/precache-manifest.9fdb2fc304dec795dc8385992998cba1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("www.in-bed.org/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
